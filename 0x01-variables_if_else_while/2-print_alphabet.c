7a73ab55c72e5bae0c05075c1301d9cbfec1980e
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-10 15:17:53 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ * Return: Always 0 (Successfully)
+ */
+int main(void)
+{
+	char c == 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar( '\n' );
+	return (0);
+}
