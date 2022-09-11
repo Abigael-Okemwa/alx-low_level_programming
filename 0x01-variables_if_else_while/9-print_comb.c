5a6b9e5d23e33ad390ef341256bc325d79c75269
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-11 02:56:02 -0700

    unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ * prints all possible combinations of single-digit numbers
+ * Return - Always 0 (done)
+ */
+
+int main(void)
+{
+	int c = 48;
+
+	while (c < 58)
+	{
+		putchar(c);
+		if (c !=57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
+
