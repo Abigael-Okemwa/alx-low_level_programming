bb7d882992156a5b01adfbf23d1019e3a80ca951
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-10 17:38:31 -0700

    still lower caes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Startingpoint
+ * program that prints the lowercase alphabet in reverse
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	char t = 'z';
+
+	while (t >= 'a')
+	{
+		putchar(c);
+		t--;
+	}
+	putchar('\n');
+	return (0);
+}
+
