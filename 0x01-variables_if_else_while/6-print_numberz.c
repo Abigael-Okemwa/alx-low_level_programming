31faea4670d4a32da09c530bc42865a2e0b2e0f7
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-10 17:23:12 -0700

    Mumberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ * single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	int f = 48;
+
+	while (f <58)
+	{
+		putchar(f);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
