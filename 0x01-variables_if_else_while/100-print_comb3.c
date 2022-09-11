c5c350f2c5e01dc87580fa8634ff926f4907fb97
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-11 12:23:49 -0700

    twos still

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - startingPoint
+ * prints all possible different combinations of two digits
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	int a = 48, b = 48;
+
+	while (a < 58)
+	{
+		b = 48;
+		while (b < 58)
+		{
+			if (a != b && a < b)
+			{
+				putchar(a);
+				putchar(b);
+				if (!(a == 56 && b ==57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
