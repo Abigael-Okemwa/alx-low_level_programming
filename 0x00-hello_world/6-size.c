09bdb875b505f51b473277e73112e730dbde4ecc
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-11 07:08:06 -0700

    variablrs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	char c;
-	int i;
-	long int a;
-	long long int b;
-	float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("size of a char: %b byte(s)\n", sizeof(c));
-	printf("size of an int: %b byte(s)\n", sizeof(i));
-	printf("size of a long int: %b byte(s)\n", sizeof(a));
-	printf("size of a long long int: %b byte(s)\n", sizeof(b));
-	printf("size of a float: %b byte(s)\n",sizeof(f));
+	printf("size of a char: %d byte(s)\n", sizeof(a));
+	printf("size of an int: %d byte(s)\n", sizeof(b));
+	printf("size of a long int: %d byte(s)\n", sizeof(c));
+	printf("size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("size of a float: %d byte(s)\n",sizeof(e));
 	return (0);
 }
 
