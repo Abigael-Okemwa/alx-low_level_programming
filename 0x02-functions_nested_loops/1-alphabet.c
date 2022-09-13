ffd6077a26bbda0c65cafb79456e196c3dff6f33
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-13 13:50:22 -0700

    when i cant fall asleep i play the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * alphabets - lowercase
+ * Return: Always 0 (Successful)
+ */
+
+void print_alphabet(void)
+{
+	char f;
+
+	for (f= 'a'; f <= 'z'; f++);
+	{
+		_putchar(f);
+	}
+	_putchar('\n');
+}
+
