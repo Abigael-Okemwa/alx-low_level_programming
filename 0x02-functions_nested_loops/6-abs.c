9b5ca729a1a426365f956be99bef08457157a763
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-14 16:15:01 -0700

    absolute values 0f n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - for the absolute integer values
+ * @n: the inegers to use
+ * Return: int
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (0);
+	}
+}
