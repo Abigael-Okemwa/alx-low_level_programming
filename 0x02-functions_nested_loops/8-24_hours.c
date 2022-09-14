d8da8fa54cee0e2bb236ff36b12f614dc4f3e89b
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-14 16:58:14 -0700

    time in minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * jack_bauer - tracks every minute
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int n = 0;
+	int a = 0, b = 0, c = 0, d = 0;
+
+	while (n < 1440)
+	{
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+
+		d++;
+		if (d > 9)
+		{
+			d = 0;
+			c++;
+		}
+		if (c > d)
+		{
+			c = 0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		n++;
+	}
+}
