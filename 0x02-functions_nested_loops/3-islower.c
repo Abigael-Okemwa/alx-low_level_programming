406e44f6fd9925f83c3b0beab36a3cec83683835
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-14 15:21:50 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - to check whether ac is lowercase or not
+ * The standard library provides a similar function
+ * Return: 1 if it is in oercase otherwise 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
