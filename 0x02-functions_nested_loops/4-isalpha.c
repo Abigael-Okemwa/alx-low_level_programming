945957d8c7a8c822a3fd5198ebbf2ac1f7d63dff
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-14 15:48:46 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether a character is part of alphabets
+ * @c: for ascii value
+ * Return: 1 if it is lowercase, if not then 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <=123) || (c >= 65 && c <= 91))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
