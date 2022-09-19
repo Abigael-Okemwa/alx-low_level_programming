16948cb76214caf3fb91ed0440df3fe29244f40e
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-19 15:43:55 -0700

    getting data through the scr

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * @src: the pointer to origin
+ * @dest: pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int cpy = 0;
+
+	while (*(scr + cpy) != '\0')
+	{
+		*(dest + cpy) = *(scr + cpy);
+		cpy++;
+	}
+	*(dest + cpy) = '\0';
+	return (dest)
+}
