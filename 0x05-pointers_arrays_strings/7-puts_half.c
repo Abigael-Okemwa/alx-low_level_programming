5ff27dfccc23aeeb5ea299bf2b1fae9db14b1556
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-19 12:22:25 -0700

    second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) = '\0')
+	{
+		if (i % 2 ==0)
+			putchar(*(str + i));
+		i++;
+	}
+	putchar (10);
+}
