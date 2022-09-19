52941c6bddfb4b229cdc56ddf149ada82328030d
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-19 07:58:41 -0700

    changing pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,17 +3,17 @@
 
 /**
  * reset_to_98 - updates the value it points
- * @ u: the value we will use
+ * @ n: the value we will use
  * Return: Always 0
  */
 
 void reset_to_98(int *n)
 {
-	int u;
+	int n;
 
-	u = 402;
-	printf("u = %d\n", u);
-	reset_to_98(&u);
-	printf("n = %d\n", u);
-	return (0);
+	n = 402;
+	printf("u = %d\n", n);
+	reset_to_98(&n);
+	printf("n = %d\n", n);
+	return (98);
 }
