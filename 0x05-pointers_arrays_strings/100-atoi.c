872178c3e1cb37eee244724b4d506e9144188303
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-19 23:12:42 -0700

    string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: convert pointer
+ * Return: an integer
+ */
+
+int _atoi(char *s)
+{
+	int u = 0;
+	int min = 1;
+	int str = 0;
+	unsigned int uns = 0;
+
+	while (s[u])
+	{
+		if (s[u] == 45)
+		{
+			min *= -1;
+		}
+		while (s[u] >= 48 && s[u] <= 57)
+		{
+			str = 1;
+			uns = (uns * 10) + (s[u] - '0');
+			u++;
+		}
+		if (str == 1)
+		{
+			break;
+		}
+		u++;
+	}
+	uns *= min;
+	return (uns)
+}
