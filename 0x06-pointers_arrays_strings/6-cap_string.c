ba872367a06faa75c75e7bc8e07c3abf84c05105
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-22 13:44:35 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,21 +15,21 @@ char *cap_string(char *)
 		while (!(str[index] >= 'a' && str[inde] <= 'z'))
 			index++;
 		if (str[index - 1] == ' ' ||
-				stir[index - 1] == '\t' ||
-				stir[index - 1] == '\n' ||
-				stir[index - 1] == ',' ||
-				stir[index - 1] == ';' ||
-				stir[index - 1] == '.' ||
-				stir[index - 1] == '!' ||
-				stir[index - 1] == '?' ||
-				stir[index - 1] == '"' ||
-				stir[index - 1] == '(' ||
-				stir[index - 1] == ')' ||
-				stir[index - 1] == '{' ||
-				stir[index - 1] == '}' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
 				index == 0)
-			stir[index] -= 32;
+			str[index] -= 32;
 		index++;
 	}
-	return(str);
+	return (str);
 
