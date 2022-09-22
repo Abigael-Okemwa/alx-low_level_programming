f4ecc028a085314059bc5f37bbe6057c17ee0bee
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-22 13:31:39 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,35 @@
 #include "main.h"
 
-/
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to be capitalized
+ * Return: str
+ */
+
+char *cap_string(char *)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[inde] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+				stir[index - 1] == '\t' ||
+				stir[index - 1] == '\n' ||
+				stir[index - 1] == ',' ||
+				stir[index - 1] == ';' ||
+				stir[index - 1] == '.' ||
+				stir[index - 1] == '!' ||
+				stir[index - 1] == '?' ||
+				stir[index - 1] == '"' ||
+				stir[index - 1] == '(' ||
+				stir[index - 1] == ')' ||
+				stir[index - 1] == '{' ||
+				stir[index - 1] == '}' ||
+				index == 0)
+			stir[index] -= 32;
+		index++;
+	}
+	return(str);
+
