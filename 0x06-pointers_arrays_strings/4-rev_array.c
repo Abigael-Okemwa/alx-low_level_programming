ee67dea1e4ed1f4af3a38d092f435fdaeb5e094d
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-22 07:29:39 -0700

    reversed string

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reverse_array -  reverses the content of an array of integers
+ * reverse_array - reverses the content of an array of integers
  * @n: is the number of elements of the array
  * @a: array of integers to be reversed
  */
