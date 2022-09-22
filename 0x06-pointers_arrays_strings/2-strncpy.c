3a13bdb884ff4a48af10a66886f5317a89230990
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-22 01:06:18 -0700

    copies of a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,6 +15,8 @@ char *_strncpy(char *dest, char *src, int n)
 	while (src[index++])
 		srcLen++;
 	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	for (index = srcLen; index < n; index++)
 		dest[index] = '\0';
 	return (dest);
 }
