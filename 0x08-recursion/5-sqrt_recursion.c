2376ec33b5c466f03cc24342a5fb612b4b64be98
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-28 14:05:09 -0700

    check for the square of a root number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * _sqrt - returns the square of a root number
+ * @n: the number to be teste
+ * @x: the number that is squared
+ * Return: the square root of n
+ */
+
+int _sqrt(int n, int x)
+{
+	if (n * n ==x)
+		return (n);
+	if (n * n > x)
+		return (-1);
+	return (_sqrt(n + 1, x));
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: number that is to be used
  * Return: the square root of n
@@ -11,19 +26,4 @@ int _sqrt_recursion(int n)
 	if (n == 0)
 		return (0);
 	return (_sqrt(1, n));
-}
-
-/**
- * _sqrt - returns the square of a root number
- * @n: the number to be tested
- * @x: the number that is squared
- * Return: the square root of n
- */
-int _sqrt(int n, int x)
-{
-	if (n * n == x)
-		return (n);
-	if (n * n > x)
-		return (-1);
-	return (_sqrt(n + 1, x));
 }
