e5148519ec09140ebacd403905c1bb96204c23bc
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-09-28 12:03:56 -0700

    x raised to y

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,3 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: integer
- * Return: the natural square root
- */
-
-int _sqrt_recursion(int n)
-{
-	int i = 0;
-
-	if (i < 0)
-		return (-1);
-	else
-	{
-		return (_evaluate(i, n);
-				}
-				}
