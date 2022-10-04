1c814a07a3e499aa7fc1c05c65d3f8f862429c04
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-10-04 10:34:06 -0700

    prints the name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * @argc: number of commandline arguments
+ * @argv: vector arguments for string pointers
+ * Return: 0
+ */
+
+int main (int argc , char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
