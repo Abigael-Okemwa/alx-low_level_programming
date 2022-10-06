84523cac3c84850c31ce64c54cae20c1f9f53e96
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-10-06 13:26:01 -0700

    using malloc to allocate memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,23 +3,22 @@
 
 /**
  * _calloc - allocates memory for an array, using malloc
- * @nmeb: the number of elements
+ * @nmemb: the number of elements
  * @size: the byte size of each array element
  * Return: if nmeb is 0, then calloc returns NULL
  * if malloc fails then calloc returns NULL
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *memb;
+	char *m;
 	unsigned int index;
 
 	if (nmemb == 0 || size == 0)
 		return(NULL);
-	memb = malloc(size * nmemb);
-	if (memb == NULL)
+	m = malloc(size * nmemb);
+	if (m == NULL)
 		return (NULL);
-
 	for (index = 0; index < (size * nmemb); index++)
-		memb[index] = 0;
-	return (meb);
+		m[index] = 0;
+	return (m);
 }
