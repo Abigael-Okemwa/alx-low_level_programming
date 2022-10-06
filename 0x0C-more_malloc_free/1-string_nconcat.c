bcd7f5d564ec579f4efad69b6dad84ae68f6118e
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-10-06 12:36:39 -0700

    conc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * string_nconcat - function that concatenates two strings
@@ -10,34 +11,23 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int sz1 = 0, sz2 = 0, 1;
+	char *conc;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[sz1] != '\0')
-	{
-		sz1++;
-	}
-	while (s2[sz2] != '\0')
-	{
-		sz2++;
-	}
-	if (n > sz2)
-		n = sz2;
-	p = malloc((sz1 + n + 1) * sizeof(char));
-	if (p == NULL)
-		return (0);
-	for (c = 0; c < sz1; c++)
-	{
-		p[c] = s1[c];
-	}
-	for (; c < (sz1 + n); c++)
-	{
-		p[c] = s2[c - sz1];
-	}
-	p[c] = '\0';
-	return (p);
+	for (index = 0; s1[index]; index++)
+		len++;
+	conc = malloc(sizeof(char) * (len + 1));
+	if (conc == NULL)
+		return (NULL);
+	len = 0;
+	for (index = 0; s1[index]; index++)
+		conc[len++] = s1[index];
+	for (index = 0; s2[index] && index < n; index++)
+		conc[len++] = s2[index];
+	conc[len] = '\0';
+	return (conc);
 }
