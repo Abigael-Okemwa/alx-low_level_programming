842fb405d8ee0dc36f7c35bf7974e25e78085e36
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-10-13 05:39:28 -0700

    tasks

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -8,5 +8,6 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
 
 #endif /* MAIN_H */
