243365d57b570ae3a5a6e58f16b76a48e3a79c09
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-10-24 13:32:46 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list
+ * @h: singly linked list
+ * Return: number of elements in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t no_of_nodes;
+
+	no_of_nodes = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		no_of_nodes++;
+	}
+	return (no_of_nodes);
+}
