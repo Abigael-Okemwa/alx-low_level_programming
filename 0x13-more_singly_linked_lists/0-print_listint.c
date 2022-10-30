034169442e2e185a155a1289ccef1303eb688e35
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-10-30 09:38:17 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h);
 {
-	size_t ncount = 0;
+	int i = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
-		ncount++;
+		i++;
 	}
-	return (ncount);
+	return (i);
 }
