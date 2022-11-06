837b6d19a43fdc63f4082102459013461c3e815c
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-11-06 00:12:30 -0700

    append text file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, 0_WRONLY | O_APPEND);
 	if (text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++);
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
 		rwr = write(fd, text_content, nletters);
 		if (rwr == -1)
 			return (-1);
