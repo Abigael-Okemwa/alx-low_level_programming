a3bb9c8e12dd04b578373389dba01351c9bd9f82
Abigael_Okemwa <abigaelbosibori2000@gmail.com>
2022-11-23 01:12:44 -0800

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,8 +3,8 @@
 
 /**
  * print_list - prints all the elements of a list_t list.
- * @h: The list_t list.
- * Return: The number of nodes in h
+ * @h: singly linked list.
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
